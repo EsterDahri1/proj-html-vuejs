5f5c5efd94fb9024fa13291e16d7522b0ce4c4a5
EsterDahri <esterdahri@gmail.com>
2023-10-03 11:40:19 +0200

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,34 @@
 
 </script>
 
-<template></template>
+<template>
+  <body>
+    <header>
+      <div class="container">
+        <div height="50px" class="social">
+          <div>
+            <span class="text-capitalize">download app</span>
+            <span class="text-capitalize">Advertise with us</span>
+          </div>
+          <div>
 
-<style scoped></style>
+          </div>
+        </div>
+      </div>
+    </header>
+    <main></main>
+    <footer></footer>
+  </body>
+</template>
+
+<style lang="scss">
+@use './assets/scss/partials/variables' as *;
+
+body {
+  background-color: $food_light;
+
+  header {
+    background-color: $food_white;
+  }
+}
+</style>
