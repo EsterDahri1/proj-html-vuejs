3fdcb6f745e3289bfb6e4aff3bd66d8cba10d91d
EsterDahri <esterdahri@gmail.com>
2023-10-03 16:53:05 +0200

    add footer structure and copyright

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,25 @@ export default {
   <body>
     <AppHeader />
     <main></main>
-    <footer></footer>
+    <footer>
+      <div class="links">
+        <div class="container">
+          <div class="row row-cols-3"></div>
+        </div>
+      </div>
+      <div class="copyright text-center">
+        <p>
+          &copy; Copyright 2012 - 2020 |
+          <a class="text-decoration-none text-capitalize" href="#">
+            Avada Website Builder
+          </a> by
+          <a class="text-decoration-none text-capitalize" href="#">
+            ThemeFusion
+          </a>
+          | All Rights Reserved | Powered by WordPress
+        </p>
+      </div>
+    </footer>
   </body>
 </template>
 
@@ -23,5 +41,26 @@ export default {
 body {
   background-color: $food_light;
   font-family: 'Catamaran', sans-serif;
+
+  footer {
+    height: 350px;
+
+    .links {
+      background-color: white;
+    }
+
+    .copyright {
+      color: $food_grey;
+      font-size: 13px;
+
+      a {
+        color: black;
+      }
+
+      a:hover {
+        color: $food_warning;
+      }
+    }
+  }
 }
 </style>
