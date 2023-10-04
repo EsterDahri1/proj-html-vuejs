104a361157144979d046fc2325438b125c8081c1
EsterDahri <esterdahri@gmail.com>
2023-10-04 08:58:29 +0200

    add structure today's pick

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,29 +5,73 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <div id="jumbotron">
-
+    <div id="jumbotron">
+        <div class="container d-flex align-items-center justify-content-center">
+            <div class="todayPick">
+                <button class="btn text-uppercase text-white">Today's picks</button>
+            </div>
         </div>
-        <section id="journal">
+    </div>
+    <section id="journal">
 
-        </section>
-        <section id="popRecipes">
+    </section>
+    <section id="popRecipes">
 
-        </section>
-        <section id="collection">
+    </section>
+    <section id="collection">
 
-        </section>
-        <section id="blog">
+    </section>
+    <section id="blog">
 
-        </section>
-        <section id="subscription">
+    </section>
+    <section id="subscription">
 
-        </section>
-        <div id="banner">
+    </section>
+    <div id="banner">
 
-        </div>
     </div>
 </template>
 
-<style></style>
+<style lang="scss">
+@use '../assets/scss/partials/variables' as *;
+
+#jumbotron {
+    height: 615px;
+    background-image: url(https://avada.website/food/wp-content/uploads/sites/118/2019/02/slide1-bg.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: 10%;
+
+    .container {
+        padding-top: 200px;
+
+        .todayPick {
+            background-color: white;
+            height: 223px;
+            width: 50%;
+            border-radius: 12px;
+            color: #67615a;
+            display: flex;
+            flex-direction: column;
+            font-size: 17px;
+            justify-content: center;
+            letter-spacing: 0.2px;
+            line-height: 30.6px;
+            margin: auto;
+            padding: 20px 0px 12px;
+            position: relative;
+
+            .btn {
+                background-color: $food_warning;
+                border-radius: 5px;
+                position: absolute;
+                bottom: 90%;
+                left: 37%;
+                font-size: 14px;
+                font-weight: 400;
+                padding: 10px 20px;
+            }
+        }
+    }
+}
+</style>
