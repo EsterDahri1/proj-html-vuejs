81307f08fb8c6011a31b7e0a9f991983d47db1d1
EsterDahri <esterdahri@gmail.com>
2023-10-05 14:58:41 +0200

    fix details app subscribe

--- src/components/AppSubscribe.vue
+++ src/components/AppSubscribe.vue
@@ -5,9 +5,9 @@ export default {
 </script>
 
 <template>
-    <section id="subscription">
-        <div class="container">
-            <div>
+    <section id="subscription" class="p-5">
+        <div class="container p-5">
+            <div width="60%" class="p-5 text-center">
                 <h3>Subscribe & Receive A Free eBook</h3>
                 <p>Aliquam erat volutpat. Ut quis ligula a magna blandit finibus. Suspendisse maximus lacus non nunc lacinia
                     lobortis.</p>
@@ -26,7 +26,10 @@ export default {
 
 #subscription {
     background-image: url(../assets/img/subscribe-sec-bg.png);
-    background-size: cover;
+    background-size: 130%;
+    object-fit: cover;
     background-color: white;
+    background-position: center;
+    height: 560px;
 }
 </style>
