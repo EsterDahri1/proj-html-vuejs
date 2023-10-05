9e1e4cc5cdc3ca10876b5010f17bd911b699e7b8
EsterDahri <esterdahri@gmail.com>
2023-10-05 09:51:31 +0200

    create cards for journal section

--- src/components/AppJournal.vue
+++ src/components/AppJournal.vue
@@ -6,8 +6,52 @@ export default {
 
 <template>
     <section id="journal">
-
+        <div class="container">
+            <div class="window">
+                <h6 class="text-uppercase">FOODIE JOURNAL</h6>
+                <div class="row">
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="img-card-top">
+                                <img src="../assets/img/" alt="">
+                            </div>
+                            <div class="card-body">
+                                <h5><a href="#">Food Corner: Top Japanese Restaurants for Sushi</a></h5>
+                                <span>By <a href="#">admin</a> | March 25th, 2019</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="img-card-top"></div>
+                            <div class="card-body">
+                                <h5><a href="#">Food Corner: Top Japanese Restaurants for Sushi</a></h5>
+                                <span>By <a href="#">admin</a> | March 25th, 2019</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="img-card-top"></div>
+                            <div class="card-body">
+                                <h5><a href="#">Food Corner: Top Japanese Restaurants for Sushi</a></h5>
+                                <span>By <a href="#">admin</a> | March 25th, 2019</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#journal {
+    height: 386px;
+
+    .window {
+        background-color: white;
+        height: 450px;
+    }
+}
+</style>
